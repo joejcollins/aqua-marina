45479792006f51a5a1367433fdc4b01ff0f3074d
Joe J Collins <j.collins@zengenti.com>
2024-06-07 08:39:55 +0100

    Just build venv

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,5 @@
 # Blow the security and upgrade
-sudo chmod -R 777 /opt/conda/pkgs/
-conda upgrade -y scikit-image
-conda install -y -c conda-forge ipympl
-make env
+# sudo chmod -R 777 /opt/conda/pkgs/
+# conda upgrade -y scikit-image
+# conda install -y -c conda-forge ipympl
+make venv
